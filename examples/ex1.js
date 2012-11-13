3233d01f3670545e28a3615a3f78bd45ca87ec98
Craig Condon <craig@spiceapps.com>
2012-11-12 23:27:52 -0600

    updated

--- examples/ex1.js
+++ examples/ex1.js
@@ -1,10 +1,11 @@
-require("../").create(require("/usr/local/etc/maestro/config"), function(err, maestro) {
+require("../")(require("/usr/local/etc/maestro/config"), function(err, maestro) {
 	if(err) console.error(err.stack);
 	maestro.sync();
 
 	maestro.getServers({ numConnections: 1 }).doesntExist(function() {
 
 	}).exec();
+
 
 
 	maestro.getServer({ numConnections: 0 }).exec(function(err, server) {
